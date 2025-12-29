bbae17e68d237c596c6f9866610ea3b243339454
Eduardo Guedes <eduardoguedesdev@gmail.com>
2025-12-29 05:29:01 -0300

    first commit

---
+++ src/test/java/com/eduardoguedes/auth_jwt/AuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoguedes.auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
